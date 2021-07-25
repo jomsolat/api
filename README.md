c21203ac80a17d46d8c521d4d9422b06452250c9
Faizal <faizal@easycrypto.nz>
2021-07-25 16:03:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Jom Solat API
+
+This API has only one endpoint:
+> http://localhost:3000/zone/:zoneId
+
+Example result:
+```json
+{
+  "_id": "60fcefc816fb5981e434aa5d",
+  "zone": "TRG04",
+  "zoneName": "Dungun, Kemaman",
+  "hijri": "1442-12-15",
+  "date": "2021-07-25",
+  "day": "Sunday",
+  "imsak": "05:42:00",
+  "fajr": "05:52:00",
+  "syuruk": "07:05:00",
+  "dhuhr": "13:17:00",
+  "asr": "16:39:00",
+  "maghrib": "19:26:00",
+  "isha": "20:39:00"
+}
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const express = require("express");
+const helmet = require("helmet");
+const app = express();
+const port = process.env.APP_PORT;
+
+app.use(helmet());
+
+const { MongoClient } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true
+});
+
+app.get('/zone/:zoneId', async function (req, res) {
+  let zone = req.params.zoneId;
+
+  const todaysDate = new Date();
+  const date = todaysDate.toISOString().split('T')[0];
+
+  await client.connect();
+	const db = client.db('jomsolat');
+	const prayerTime = db.collection('prayertime2');
+  
+  prayerTime.findOne({zone: zone, date: date}, function(err, result) {
+    if (err) throw err;
+    if (typeof result == 'undefined') result = "No result";
+    res.json(result);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`JomSolat API listening at http://localhost:${port}`);
+});
