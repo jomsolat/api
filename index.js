9116d525a35296b44d288c72fb7d453621a16777
faizalmansor <faizalmansor@gmail.com>
2021-08-07 12:51:14 +0800

    add get zones from stateId endpoint

--- index.js
+++ index.js
@@ -33,5 +33,19 @@ app.get('/zone/:zoneId', async function (req, res) {
   });
 });
 
+app.get('/zone/state/:stateId', async function (req, res) {
+  let stateId = parseInt(req.params.stateId);
+
+  await client.connect();
+  const db = client.db('jomsolat');
+  const zonecoll = db.collection('zone');
+  
+  zonecoll.find({stateId: stateId}).toArray(function(err, result) {
+    if (err) throw err;
+    if (typeof result == 'undefined') result = "No result";
+    res.json(result);
+  });
+});
+
 app.listen(port, host);
 console.log(`Running on http://${host}:${port}`);
